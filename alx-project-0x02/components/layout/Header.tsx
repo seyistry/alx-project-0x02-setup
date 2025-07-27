0ddc50c153deb4056afc651cc3e1337c5356a8b7
Oluwaseyi <seyistry@gmail.com>
2025-07-27 18:41:57 +0100

    update nav

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -7,6 +7,8 @@ const Header = () => {
       <ul className="flex gap-2 justify-center py-4">
         <li><Link href={'/home'}>Home</Link></li>
         <li><Link href={'/about'}>About</Link></li>
+        <li><Link href={'/posts'}>Post</Link></li>
+        <li><Link href={'/users'}>User</Link></li>
       </ul>
     </div>
   );
